75da2bd542f34dfc483701956a6803fe9707a182
Vic <victoria.ad40@gmail.com>
2024-07-03 12:10:19 -0300

    fix about me text

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -24,7 +24,14 @@ const AboutMeSection = styled.section`
 
 const AboutMeImg = styled.img`
   grid-row-start: span 3;
-  width: 33.5rem;
+  width: 33rem;
+`;
+
+const AboutMeInnerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2.5rem;
+  margin: 3rem 0 1.5rem;
 `;
 
 const Title = styled.h1`
@@ -32,6 +39,10 @@ const Title = styled.h1`
     1px 1px #5e262b, -1px -1px #5e262b, 1px -1px #5e262b, -1px 1px #5e262b,
     6px 6px 0px #5e262b;
   font-size: 4.375rem;
+`;
+
+const TextContainer = styled.p`
+  font-size: 1.25rem;
 `;
 
 const Home = () => {
@@ -43,18 +54,26 @@ const Home = () => {
       <Content>
         <AboutMeSection>
           <AboutMeImg src="assets/about-me.png" />
-          <Title>Hello,</Title>
-          <p>
-            I´m <span style={{ fontWeight: 800 }}>Carolina Silva da Silva</span>
-            , a seasoned designer with 6 years of expertise. Over the past 4
-            years, I have specialized as a UX/UI Product Designer in the dynamic
-            tech industry, collaborating with diverse teams from the UK, USA,
-            and Latin America. I've designed innovative international websites,
-            platforms, and mobile apps for a wide array of companies, showcasing
-            innovation and creativity in every project. You can see down below
-            some of my stronges abilities:
-          </p>
-          <div></div>
+          <AboutMeInnerContainer>
+            <Title>Hello,</Title>
+            <div>
+              <TextContainer style={{ marginBottom: "1.25rem" }}>
+                I´m{" "}
+                <span style={{ fontWeight: 800 }}>Carolina Silva da Silva</span>
+                , a seasoned designer with 6 years of expertise. Over the past 4
+                years, I have specialized as a UX/UI Product Designer in the
+                dynamic tech industry, collaborating with diverse teams from the
+                UK, USA, and Latin America. I've designed innovative
+                international websites, platforms, and mobile apps for a wide
+                array of companies, showcasing innovation and creativity in
+                every project.
+              </TextContainer>
+              <TextContainer>
+                You can see down below some of my stronges abilities:
+              </TextContainer>
+            </div>
+            <div></div>
+          </AboutMeInnerContainer>
         </AboutMeSection>
         <section></section>
         <section></section>
